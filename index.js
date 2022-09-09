fbff517d72074de300aa3c3a128ca05ef224251a
Roberto Paes <robertocpaes@gmail.com>
2022-09-08 22:59:46 -0300

    adicionado rota padrão

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const data = [
 
 
 app.get('/', function (req, res) {
-    res.send("Olá mundo, olá Dokku e adeus Heroku!!!. " + Date.now().toLocaleString())
+    res.send("Olá mundo, olá Dokku e adeus Heroku!!!. " + new Date().toLocaleString())
   })
 
 app.get('/lista', function (req, res) {
